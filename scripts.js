d8653ca873b65704a076d8401db6d925e2191fd6
MsPockets <mcnutterika@gmail.com>
2020-07-26 00:33:29 -0400

    Automate scramble generation
    generate a scramble when the page laods
    generate a new scramble every time the timer is stopped
    
    combine scramble.js and scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 let running = false
 let hundredths = 0
 let seconds = 0
@@ -15,7 +16,7 @@ function tick() {
         seconds = 0
         minutes += 1
     }
-
+    
     document.getElementById('hundredths').innerHTML = (hundredths < 10 ? '0' + hundredths : hundredths)
     document.getElementById('seconds').innerHTML = (seconds < 10 ? '0' + seconds : seconds)
     document.getElementById('minutes').innerHTML = (minutes < 10 ? '0' + minutes : minutes)
@@ -29,16 +30,17 @@ function startTimer() {
 }
 function stopTimer() {
     clearInterval(runInt)
+    generateScramble()
 }
 
 function startStopTimer() {
-  if (running) {
-    running = false
-    stopTimer()
-  } else {
-    running = true
-    startTimer()
-  }
+    if (running) {
+        running = false
+        stopTimer()
+    } else {
+        running = true
+        startTimer()
+    }
 }
 
 document.body.onkeyup = function(e) {
@@ -53,3 +55,35 @@ document.body.addEventListener('click', function(e) {
         startStopTimer()
     }
 })
+
+// Scramble Generator
+const faces = ['R', 'U', 'F', 'B', 'L', 'D']
+const rotation = ['', '2', "'"]
+const scrambleAlg = document.getElementById('scrambleAlg')
+document.getElementById("scrambleBtn").onclick = scrambleClick
+
+const  generateScramble = function() {
+    let scramble = []
+    let move
+    let lastface
+    let face
+    while (scramble.length < 25){
+        move = faces[Math.floor(Math.random() * Math.floor(6))]
+        face = move
+        move += rotation[Math.floor(Math.random() * Math.floor(3))]
+        if (lastface !== face) {
+            scramble.push(move)
+            lastface = face
+            face = ''
+        }
+        move = ''
+    }
+    scrambleAlg.innerHTML = scramble.join(' ')
+    return scramble
+}
+
+function scrambleClick(clicked) {
+    generateScramble()
+}
+
+document.getElementById('scrambleAlg').addEventListener("load", generateScramble())
